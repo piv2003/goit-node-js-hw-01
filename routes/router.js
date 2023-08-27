5fba4627e598c4be922a47f6d93370769d8466e5
Ihor Poliakov <piv2003@gmail.com>
2023-08-27 23:45:55 +0200

    adding data

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
+const fs = require("fs/promises");
+const path = require("path");
+
+const usersPath = path.join(__dirname, "../db/users.json");
 
 const router = Router();
 
-router.get("/test", (req, res) => {
-  res.json({ message: "Hello friend!" });
+router.get("/users", async (req, res) => {
+  try {
+    const users = await fs.readFile();
+  } catch (e) {}
 });
 
 module.exports = router;
