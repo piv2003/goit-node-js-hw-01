afc38454bd6741094ed9b1d0b537e29ae71dc843
Ihor Poliakov <piv2003@gmail.com>
2023-09-01 21:28:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 const { Command } = require("commander");
 const program = new Command();
-
 const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.table(oneContact);
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.table(newContact);
+
+    case "remove":
+      const deleteById = await contacts.removeContact(id);
+      return console.table(deleteById);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
