e33a486afd9e604a49f112256b3e660951d7100b
Ihor Poliakov <piv2003@gmail.com>
2023-09-01 21:35:37 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -2,3 +2,14 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find(({ id }) => id === contactId);
+  return contact || null;
+};
