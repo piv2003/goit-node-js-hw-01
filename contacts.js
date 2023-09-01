9c0bfce31061ae7c7d4b2dff924467dc539823a8
Ihor Poliakov <piv2003@gmail.com>
2023-09-01 21:36:22 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -13,3 +13,14 @@ const getContactById = async (contactId) => {
   const contact = contacts.find(({ id }) => id === contactId);
   return contact || null;
 };
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const indexContact = contacts.findIndex(({ id }) => id === contactId);
+
+  if (indexContact === -1) return null;
+
+  const [result] = contacts.splice(indexContact, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
